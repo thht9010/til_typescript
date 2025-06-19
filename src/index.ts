26ff0b9017ce9534653260263fb6177cacf2851e
thht9010 <kttpo9090@gmail.com>
2025-06-19 11:35:54 +0900

    [docs] 배열과 객체 기초

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
-let neverVar1: never = 100;
-let neverVar2: never = true;
-let neverVar3: never = undefined;
-let neverVar4: never = null;
+type Good = { title: string; price: number; sale: boolean };
+
+const goodArr: Good[] = [
+  { title: "사과", price: 1000, sale: true },
+  { title: "딸기", price: 5000, sale: false },
+  { title: "메로나", price: 500, sale: true },
+];
+
+goodArr[0].title;
+
+const goodArrGeneric: Array<Good> = [
+  { title: "사과", price: 1000, sale: true },
+  { title: "딸기", price: 5000, sale: false },
+  { title: "메로나", price: 500, sale: true },
+];
+
+goodArrGeneric[0].title;
